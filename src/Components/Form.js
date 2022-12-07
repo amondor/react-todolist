e945f15c538ed857c65bc7f5b83af925cc0d0092
Andrew Mondor <mondor.andrew@gmail.com>
2022-12-07 15:12:24 +0100

    delete todo function

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,13 +1,22 @@
 import { useState } from "react";
 import Item from './Item'
+import {v4 as uuidv4} from 'uuid'
 
 export default function Form(){
 
     const [dataArr, setDataArr] = useState ([
-        {txt: "Promener le chien"},
-        {txt: "Sport"},
-        {txt: "Coder une app react"},
+        {txt: "Promener le chien", id: uuidv4()},
+        {txt: "Sport", id: uuidv4()},
+        {txt: "Coder une app react" ,id: uuidv4()},
     ])
+    const deleteElement = id => {
+       // console.log(id);
+        const FilteredState = dataArr.filter(item => {
+            return item.id !== id;
+        })
+        setDataArr (FilteredState)
+    }
+
     return (
         <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
 
@@ -21,11 +30,13 @@ export default function Form(){
 
             <h2>List des choses à faire :</h2>
             <ul className="list-group">
-               {dataArr.map((item, index) =>{
+               {dataArr.map((item) =>{
                     return(
                         <Item  
                         txt ={item.txt}
-                        key={index}
+                        key={item.id}
+                        delFunc = {deleteElement}
+                        id = {item.id}
                 
                         />
                     )
--- src/Components/Item.js
+++ src/Components/Item.js
@@ -2,7 +2,9 @@
    return (
      <li className="border d-flex justify-content-between align-items-center p-2 m-2">
         <div className="p-3">{props.txt}</div>
-        <button className="btn btn-danger p-2 h-50">Suprrimer</button>
+        <button className="btn btn-danger p-2 h-50"
+        onClick={()=> props.delFunc(props.id)}
+        >Suprrimer</button>
      </li>
    )
  }
